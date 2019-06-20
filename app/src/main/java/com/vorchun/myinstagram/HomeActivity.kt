426ab2dfcf69054329b6b6683afbaf3a4b479293
Vor4un <Vorchun87-07@mail.ru>
2019-06-20 11:02:02 +0400

    add temporary stub for firebase

--- app/src/main/java/com/vorchun/myinstagram/HomeActivity.kt
+++ app/src/main/java/com/vorchun/myinstagram/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.vorchun.myinstagram
 
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
@@ -12,6 +13,15 @@ class HomeActivity : BaseActivity(0) {
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
 
-
+        // add temporary stub for firebase
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("Vorchun87-07@mail.ru", "password")
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    Log.d(TAG, "signIn: success")
+                } else {
+                    Log.d(TAG, "signIn: failure", it.exception)
+                }
+            }
     }
 }
